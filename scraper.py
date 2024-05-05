1dee3f678eb77e167cec215b37e4bad0ab68cea1
Marie <marie.wolfel@email.ucr.edu>
2024-05-05 12:24:56 -0700

    added file saving

--- scraper.py
+++ scraper.py
@@ -1,36 +1,52 @@
 import praw
+import requests
+import os
 
-#Linking program to our Reddit Developer Account to scrape posts & comments"
+# save HTML content to a file
+def save_html_to_file(url, file_path):
+    response = requests.get(url)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+
+# Linking program to our Reddit Developer Account to scrape posts & comments
 reddit = praw.Reddit(
     client_id="jU9lQ5ZNRqRV_VJ0MzhKaQ",
     client_secret="JIUYlpI18PiSeT5MgiGzSb9c3HYzaA",
     user_agent="CS172 my-app scraper",
 )
 
-subreddit = reddit.subreddit("HydroHomies") #subreddit name == "HydroHomies"
-top_posts = subreddit.top(limit=10) #limits to 10 posts
-new_posts = subreddit.new(limit=10)
+subreddit = reddit.subreddit("AmIOverreacting")  # Subreddit name == "HydroHomies"
+top_posts = subreddit.top(limit=10)  # Limits to 10 posts
 
-#Print out details of first 10 posts in the HydroHomies subreddit
-for post in top_posts:  #more details in praw documentation
+# Create a directory to store crawled HTML pages if it doesn't exist
+if not os.path.exists('crawled_pages'):
+    os.makedirs('crawled_pages')
+
+# Iterate over top posts
+for post in top_posts:
     print("Title - ", post.title)
     print("ID - ", post.id)
     print("Author - ", post.author)
     print("URL - ", post.url)
     print("Score - ", post.score)
     print("Comment count - ", post.num_comments)
-    print("Created - ", post.created_utc) #returns timestamp of post
-    print()
+    print("Created - ", post.created_utc)
+
+    # Save HTML content to a file
+    file_name = f"crawled_pages/{post.id}.html"
+    save_html_to_file(post.url, file_name)
+
+    print("HTML content saved to:", file_name)
+    print('----------')
 
 print('----------')
-post = reddit.submission(id="l0wpxj") #grabbing one of the ids of a post
+post = reddit.submission(id="l0wpxj")  # Grabbing one of the ids of a post
 comments = post.comments
 
-for comment in comments[:2]: #grab first 2 comments of that specific post
+for comment in comments[:2]:  # Grab first 2 comments of that specific post
     print("Printing comment...")
     print("Comment body - ", comment.body)
     print("Author - ", comment.author)
     print()
 
 #print(reddit.read_only)
-
